59c1e23fe733c49a255f745fe05e1e5cc0f91756
Gianluca <panzaguardattigianluca@gmail.com>
2025-04-08 14:27:02 -0300

    Updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Theme } from '@radix-ui/themes'
+import '@radix-ui/themes/styles.css'
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Theme>
+      <App />
+    </Theme>
   </StrictMode>,
 )
