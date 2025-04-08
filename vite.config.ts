1ba2107f970434603df44a1ca9a3f0407b8d4f13
Gianluca <panzaguardattigianluca@gmail.com>
2025-04-08 15:13:56 -0300

    Update?

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { splitVendorChunkPlugin } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    splitVendorChunkPlugin()
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -20,14 +16,11 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
-    minify: 'esbuild',
-    cssMinify: true,
     rollupOptions: {
       output: {
         manualChunks: {
           'react-vendor': ['react', 'react-dom'],
           'ui-vendor': ['@radix-ui/themes', 'lucide-react'],
-          'styles': ['@radix-ui/themes/styles.css']
         },
       },
     },
